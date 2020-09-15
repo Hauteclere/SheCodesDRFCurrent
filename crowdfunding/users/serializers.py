68049dd1230e9314610193149db016e8da771782
Oliver Lavers <oliver.f.lavers@gmail.com>
2020-09-15 15:46:43 +1000

    modified the user serializer to allow it to set a password on account creation

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -4,7 +4,12 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.Serializer):
 	id = serializers.ReadOnlyField()
 	username = serializers.CharField(max_length=200)
-	email = serializers.CharField(max_length=200)
+	email=serializers.CharField(max_length=200)
+	password=serializers.CharField(style={'input_type': 'password'}, write_only = True)
 	
 	def create(self, validated_data):
-		return CustomUser.objects.create(**validated_data)
+		password = validated_data.pop('password')		
+		user =  CustomUser.objects.create(**validated_data)
+		user.set_password(password)
+		user.save()
+		return user
