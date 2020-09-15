b0619dd33eb7f4f4b3ef7f339c6ddaddf5f922cd
Oliver Lavers <oliver.f.lavers@gmail.com>
2020-09-15 16:05:06 +1000

    tweaked the user serializer again to prevent a common error mode

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -4,12 +4,12 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.Serializer):
 	id = serializers.ReadOnlyField()
 	username = serializers.CharField(max_length=200)
-	email=serializers.CharField(max_length=200)
-	password=serializers.CharField(write_only = True)
+	email = serializers.CharField(max_length=200)
+	password = serializers.CharField(write_only = True)
 	
 	def create(self, validated_data):
 		password = validated_data.pop('password')		
-		user =  CustomUser.objects.create(**validated_data)
+		user = CustomUser(**validated_data)
 		user.set_password(password)
 		user.save()
 		return user
